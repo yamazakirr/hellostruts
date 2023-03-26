06ef7544b8c9e2897389c5dc09cff0cbe7ffdf0d
yamazaki <yamazaki0317@icloud.com>
2023-03-27 07:29:30 +0900

    sample

---
+++ src/com/sample/action/HelloStruts.java
@@ -0,0 +1,11 @@
+package com.sample.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloStruts extends ActionSupport{
+
+	public String execute(){
+		return SUCCESS;
+	}
+
+}
